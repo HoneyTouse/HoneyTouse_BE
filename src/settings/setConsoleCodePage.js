4d5bc566f7fc94e76e199aab2a7c1799634262cb
devellybutton <leegr0106@gmail.com>
2024-12-10 13:47:17 +0900

    [🐛fix]: setConsoleCodePage에서 개발환경에서만 ffi-napi 불러오도록 함

--- src/settings/setConsoleCodePage.js
+++ src/settings/setConsoleCodePage.js
@@ -1,14 +1,17 @@
-const ffi = require('ffi-napi');
 const os = require('os');
 
-if (os.platform() === 'win32') {
-  const kernel = ffi.Library('Kernel32', {
-    'SetConsoleOutputCP': [ffi.types.bool, [ffi.types.uint]],
-    'SetConsoleCP': [ffi.types.bool, [ffi.types.uint]],
-  });
+if (process.env.NODE_ENV !== 'production') {
+  const ffi = require('ffi-napi');
 
-  const CP_UTF8 = 65001;
-  
-  kernel.SetConsoleOutputCP(CP_UTF8);
-  kernel.SetConsoleCP(CP_UTF8);
+  if (os.platform() === 'win32') {
+    const kernel = ffi.Library('Kernel32', {
+      'SetConsoleOutputCP': [ffi.types.bool, [ffi.types.uint]],
+      'SetConsoleCP': [ffi.types.bool, [ffi.types.uint]],
+    });
+
+    const CP_UTF8 = 65001;
+
+    kernel.SetConsoleOutputCP(CP_UTF8);
+    kernel.SetConsoleCP(CP_UTF8);
+  }
 }
